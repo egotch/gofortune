2991da75d622fc242b38127836155d267cb4ddd1
Eric Gotkowski <egotkowski@gmail.com>
2024-09-21 23:51:13 -0500

    added function to identify all fortune files

--- fortune.go
+++ fortune.go
@@ -2,29 +2,74 @@ package main
 
 import (
 	"bufio"
-	"fmt"
+	"log"
+	"os"
 	"os/exec"
+	"path/filepath"
 	"strings"
 )
 
+var files []string
+
+// visit 
+// searchescurrent path for valid fortunes to store for use later
+func visit(path string, f os.FileInfo, err error)error  {
+  
+  if err != nil{
+    log.Fatal(err)
+  }
+  // excluded "offensive" fortunes
+  if strings.Contains(path, "/off/"){
+    return nil
+  }
+
+  // exclued *.dat files
+  if strings.Contains(path, ".dat"){
+    return nil
+  }
+
+  if strings.Contains(path, ".u8"){
+    return nil
+  }
+
+  // excluded any sub dirs
+  if f.IsDir(){
+    return nil
+  }
+
+  files = append(files, path)
+  return nil
+
+}
+
+
+// main - main entry point
 func main()  {
 
   // define the command to run
   fortuneCommand := exec.Command("fortune", "-f")
   pipe, err := fortuneCommand.StderrPipe()
   if err != nil {
-    panic(err)
+    log.Panic(err)
   }
 
   // kick off the command and grab the stderr output
   fortuneCommand.Start()
   outputStream := bufio.NewScanner(pipe)
   outputStream.Scan()
-  fmt.Println(outputStream.Text())
+  log.Println(outputStream.Text())
 
   // get the path from the returned line
   line := outputStream.Text()
-  path := line[strings.Index(line, "/"):]
-  fmt.Println(string(path))
+  root := line[strings.Index(line, "/"):]
+
+  // walk the root dir of the fortunes
+  err = filepath.Walk(root, visit)
+  if err != nil {
+    log.Panic(err)
+  }
+
+  // log the file contents
+  log.Println(len(files))
 }
 
